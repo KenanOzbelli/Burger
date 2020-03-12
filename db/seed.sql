270fdc2f4e53db510479fd1d5237ed6261b5ff49
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2020-03-12 18:41:32 -0400

    wrote some awesome things

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("CHEESEBURGER", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("GOODBURGER", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BACON CHEESEBURGER", 1);
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'))
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+const connection = mysql.createConnection({
+    host:"localhost",
+    port:3306,
+    user:"root",
+    password:"root",
+    database:"burgers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
+  });
+  app.get("/", function(req,res){
+      connection.query("SELECT * FROM burgers;", function(err,data){
+          if(err){
+              throw err;
+          }
+          res.render("index", {burgers: data});
+      })
+  })
+  app.put('/api/burger/:id', (req,res)=>{
+      const id  = req.params.id
+    connection.query("UPDATE burgers SET ? WHERE ?",
+    [
+      {
+        devoured: 1
+      },
+      {
+        id: id
+      }
+    ], function(err,data){
+      if (err){
+          console.log(err)
+          res.sendStatus(500)
+          return
+      }
+      console.log(data)
+      res.sendStatus(200)
+  })})
+  app.post('/api/burger', (req, res)=>{
+      connection.query("INSERT INTO burgers SET ?",
+     req.body,
+      function(err, resaults) {
+        if (err) throw err;
+        res.sendStatus(200)
+      })
+  })
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
