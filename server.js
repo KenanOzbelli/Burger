4b39bbb45fd1cd598e53a2e4f92dfdccf05d3e87
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2020-03-12 18:55:15 -0400

    added jawsdb

--- server.js
+++ server.js
@@ -12,14 +12,19 @@ app.use(express.static('public'))
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-const connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host:"localhost",
     port:3306,
     user:"root",
     password:"root",
     database:"burgers_db"
 });
-
+};
 connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
